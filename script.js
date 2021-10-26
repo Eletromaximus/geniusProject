78467df429448c4d7dbf092bddc3d5be754c11f9
Max Milliano de Souza Machado <maxmillianox@protonmail.com>
2021-10-26 20:42:26 -0300

    parte 4

--- script.js
+++ script.js
@@ -12,6 +12,7 @@ const red = document.querySelector('.red');
 const green = document.querySelector('.green');
 const yellow = document.querySelector('.yellow');
 
+// cria ordem aleatória
 let shuffleOrder = () => {
   let colorOrder = Math.floor(Math.random() * 4);
   order[order.length] = colorOrder;
@@ -21,13 +22,38 @@ let shuffleOrder = () => {
     lightColor(elementColor, Number(i) + 1);
   }
 }
-
+// acende a proxima cor
 let lightColor = (element, number) => {
   number = number * 500;
   setTimeout(() => {
     element.classList.add('selected')
   }, tempo - 250);
   setTimeout(() => {
-    element.classList.
+    element.classList.remove('selected')
+  });
+}
+// verifica a ordem dos botões clicados
+let checkOrder = () => {
+  for(let i in clickedOrder) {
+    if(clickedOrder[i] != order[i]) {
+      lose();
+      break;
+    }
+  }
+  if(clickedOrder.length == order.length) {
+    alert(`Pontuação: ${score}\nVocê acertou! Inicando próximo nível!`);
+    nextLevel();
+  }
+}
+
+// função para o clique do usuario
+let click = (color) => {
+  clickedOrder(clickedOrder.length) = color;
+  createColorElement(color).classList.add('selected');
+
+  setTimeout(() => {
+    createColorElement(color).classList.remove('selected');
   })
+
+  checkOrder();
 }
