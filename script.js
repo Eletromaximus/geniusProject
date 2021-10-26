508314211ef7cc92ee1ca28970cdc1307ea712b2
Max Milliano de Souza Machado <maxmillianox@protonmail.com>
2021-10-26 20:31:25 -0300

    parte 3

--- script.js
+++ script.js
@@ -17,6 +17,17 @@ let shuffleOrder = () => {
   order[order.length] = colorOrder;
 
   for(let i in order) {
-    let elementColor = 
+    let elementColor = createColorElement(order[i]);
+    lightColor(elementColor, Number(i) + 1);
   }
+}
+
+let lightColor = (element, number) => {
+  number = number * 500;
+  setTimeout(() => {
+    element.classList.add('selected')
+  }, tempo - 250);
+  setTimeout(() => {
+    element.classList.
+  })
 }
